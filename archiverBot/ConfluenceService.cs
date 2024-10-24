3d75c1373f129ef9cdd56986bd4dc2ab1288268f
Toivo Järvi <toivo.jarvi@koud.fi>
2024-10-24 13:00:59 +0300

    Increment page version

--- archiverBot/ConfluenceService.cs
+++ archiverBot/ConfluenceService.cs
@@ -31,11 +31,15 @@ public class ConfluenceService
 
     public async Task PostToConfluence(string title, string content)
     {
+        
+        var currentVersion = await GetCurrentPageVersion();
+        var newVersion = currentVersion + 1;
+        
         var url = $"{_baseUrl}/wiki/rest/api/content/{_pageId}";
         
         var body = new
         {
-            version = new { number = 2 }, 
+            version = new { number = newVersion }, 
             title = title,
             type = "page",
             body = new
@@ -56,12 +60,32 @@ public class ConfluenceService
         if (response.IsSuccessStatusCode)
         {
             Console.WriteLine("Successfully posted to Confluence!");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Response from Confluence: {responseContent}");
         }
         else
         {
             Console.WriteLine($"Failed to post to Confluence: {response.StatusCode}");
             var error = await response.Content.ReadAsStringAsync();
             Console.WriteLine(error);
+        }
+    }
+    
+    public async Task<int> GetCurrentPageVersion()
+    {
+        var url = $"{_baseUrl}/wiki/rest/api/content/{_pageId}";
+        var response = await _httpClient.GetAsync(url);
+    
+        if (response.IsSuccessStatusCode)
+        {
+            var responseData = await response.Content.ReadAsStringAsync();
+            var jsonResponse = JsonConvert.DeserializeObject<dynamic>(responseData);
+            int currentVersion = jsonResponse.version.number;
+            return currentVersion;
+        }
+        else
+        {
+            throw new Exception($"Failed to get current version: {response.StatusCode}");
         }
     }
 }
